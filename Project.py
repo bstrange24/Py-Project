d23500f4d4bbc76134896e3cbe02f99b13bdf0e0
hduser <bstrange@yahoo.com>
2017-11-26 15:13:30 -0500

    Version 1.2

--- Project.py
+++ Project.py
@@ -13,6 +13,7 @@ fmt = '%(asctime)s %(levelname)s %(lineno)s %(message)s'
 logging.basicConfig(level='INFO', format=fmt, filename=r'C:\Users\hduser\PycharmProjects\Project\Project.log', datefmt='%m-%d-%Y %I:%M:%S %p')
 logger = logging.getLogger('Project')
 logger.info("*** Starting Project ***")
+logger.info(time.clock())
 
 # Initialize list and maps
 pitching_table = []
@@ -24,11 +25,12 @@ baseball_ref_mapping = {}
 # Header information for consolidated file
 baseball_reference_header = 'Name,Age,Tm,Lg,W,L,WL_Perct,ERA,G,GS,GF,CG,SHO,SV,IP,H,R,ER,HR,BB,IBB,SO,HBP,BK,WP,BF,ERA_Plus,FIP,WHIP,H9,HR9,BB9,SO9,SO_W,'
 fan_graph_header = 'FB_Perct,FB_Vel,SL_Perct,SL_Vel,CT_Perct,CT_Vel,CB_Perct,CB_Vel,CH_Perct,CH_Vel,SF_Perct,SF_Vel,KN_Perct,KN_Vel,XX_Perct'
+#fan_graph_header = 'FB_Perct'#,FB_Vel,SL_Perct,SL_Vel,CT_Perct,CT_Vel,CB_Perct,CB_Vel,CH_Perct,CH_Vel,SF_Perct,SF_Vel,KN_Perct,KN_Vel,XX_Perct'
 columns = baseball_reference_header + fan_graph_header
 
 # Years and pages for the fan graph and baseball reference website
 pages = list(range(1, 11))
-years = list(range(2017, 2018))
+years = list(range(2007, 2018))
 
 # Make any empty fields zero '0'
 def zero_out_empty_fields(field):
@@ -43,10 +45,18 @@ def replace_unicode_chars(field):
     filtered = field.replace(u'\xa0', ' ')
     return filtered
 
-# Loop over all the years from 2000 to 2017
+# Loop over all the years from 2007 to 2017
 for year in years:
+
+    # Set fan graph url to the year that is getting processed
     fan_graph_url = 'http://www.fangraphs.com/leaders.aspx?pos=all&stats=pit&lg=all&qual=30&type=4&season=' + str(year) + '&month=0&season1=' + str(year) + '&ind=0&team=0&rost=0&age=17,58&filter=&players=0&page='
-    logger.info("Base URL :: " + str(fan_graph_url))
+
+    # Set baseball reference url to the year that is getting processed
+    baseball_ref_url = 'http://www.baseball-reference.com/leagues/MLB/' + str(year) + '-standard-pitching.shtml'
+
+    logger.info("Fan Graph URL :: " + str(fan_graph_url))
+    logger.info("Baseball Reference URL :: " + str(baseball_ref_url))
+
     # Consolidated file with fan graph and baseball reference data
     consolidated_file = r'C:\Users\hduser\Desktop\project\consolidated\\' + str(year) + '_consolidated.csv'
 
@@ -90,44 +100,50 @@ for year in years:
         logger.error("Connection error occurred :: " + str(connection_error))
         raise
 
-    # Set baseball reference url to the year that is getting processed
-    baseball_ref_url = 'http://www.baseball-reference.com/leagues/MLB/' + str(year) + '-standard-pitching.shtml'
-    # Open baseball reference url with BeautifulSoup
-    soup = BeautifulSoup(urllib.request.urlopen(baseball_ref_url).read(), "html.parser")
-    # Extract comments
-    comments = soup.find_all(string=lambda text: isinstance(text, Comment))
-    elements = '<b>' + str(comments[15]) + '<b>'
-    # Parse baseball reference data
-    baseball_ref_soup = BeautifulSoup(elements.encode("utf-8", "ignore"), "html.parser")
 
-    # Loop over parsed baseball reference data and get the players names and append them to the baseball reference list
-    for element in baseball_ref_soup.find_all("tr", {"class": "full_table non_qual"}):
-        for name in element.find_all("td"):
-            player_name = replace_unicode_chars(name.text)
-            baseball_ref_data.append(player_name)
+    try:
+        # Open baseball reference url with BeautifulSoup
+        soup = BeautifulSoup(urllib.request.urlopen(baseball_ref_url).read(), "html.parser")
+        # Extract comments
+        comments = soup.find_all(string=lambda text: isinstance(text, Comment))
+        elements = '<b>' + str(comments[15]) + '<b>'
+        # Parse baseball reference data
+        baseball_ref_soup = BeautifulSoup(elements.encode("utf-8", "ignore"), "html.parser")
 
-        # Remove any special characters that the baseball reference website uses
-        remove = string.punctuation
-        pattern = r"[{}]".format(remove)
-        name_filtered = re.sub(pattern, "", baseball_ref_data[0])
-        #name_filtered = name_filtered.replace(u'\xa0', ' ')
-        # Add baseball reference data to the baseball reference map
-        baseball_ref_mapping[name_filtered] = baseball_ref_data[1:]
-        # Clear map for the next iteration
-        baseball_ref_data.clear()
+        # Loop over parsed baseball reference data and get the players names and append them to the baseball reference list
+        for element in baseball_ref_soup.find_all("tr", {"class": "full_table non_qual"}):
+            for name in element.find_all("td"):
+                player_name = replace_unicode_chars(name.text)
+                baseball_ref_data.append(player_name)
 
-    # Loop over values in the fan graph map
-    for key in fan_graph_mapping:
-        # If the fan graph map key (players name) is in the baseball reference map,
-        # add the baseball reference map value to the temporary dictionary
-        if key in baseball_ref_mapping:
-            temp_dict[key] = baseball_ref_mapping.get(key)
+            # Remove any special characters that the baseball reference website uses
+            remove = string.punctuation
+            pattern = r"[{}]".format(remove)
+            name_filtered = re.sub(pattern, "", baseball_ref_data[0])
+            name_filtered = name_filtered.replace(u'\xa0', ' ')
+            # Add baseball reference data to the baseball reference map
+            baseball_ref_mapping[name_filtered] = baseball_ref_data[1:]
+            # Clear map for the next iteration
+            baseball_ref_data.clear()
 
-            # Loop over the fan graph map and append the data to the temporary dictionary.
-            # The temporary dictionary will have the data from both websites keyed on the players name
-            if temp_dict:
-                for keys in fan_graph_mapping.get(key):
-                    temp_dict[key].append(zero_out_empty_fields(keys.rstrip(' %')))
+        # Loop over values in the fan graph map
+        for key in fan_graph_mapping:
+            # If the fan graph map key (players name) is in the baseball reference map,
+            # add the baseball reference map value to the temporary dictionary
+            if key in baseball_ref_mapping:
+                temp_dict[key] = baseball_ref_mapping.get(key)
+
+                # Loop over the fan graph map and append the data to the temporary dictionary.
+                # The temporary dictionary will have the data from both websites keyed on the players name
+                if temp_dict:
+                    for keys in fan_graph_mapping.get(key):
+                        temp_dict[key].append(zero_out_empty_fields(keys.rstrip(' %')))
+    except ConnectionError as connection_error:
+        logger.error("Connection error occurred :: " + str(connection_error))
+        raise
+    except RuntimeError as run_time_error:
+        logger.error("Runtime error occurred :: " + str(run_time_error))
+        raise
 
     # Open consolidated output file for writing
     with open(consolidated_file, "a") as output_file:
